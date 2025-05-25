6a3279331fe4625ab76d255b046bc0b049acd68c
d3vshoaib <shoaib.khan200499@gmail.com>
2025-05-25 19:26:05 +0530

    static home page design

--- src/main.js
+++ src/main.js
@@ -3,6 +3,39 @@ import { initPWA } from "./pwa.js";
 
 const app = document.querySelector("#app");
 app.innerHTML = `
-  <div></div>
+  <div class="shome-container">
+    <div class="box"></div>
+    <div class="box-title">
+      <img class="title-svg" src="/favicon.svg" alt="Shome Logo" />
+    </div>
+    <div class="box"></div>
+    <div class="box-mid"></div>
+    <div class="box-mid">
+      <input
+        class="search-input"
+        type="text"
+        placeholder="Search with !bangs..."
+        id="search-input"
+      />
+    </div>
+    <div class="box-mid"></div>
+    <div class="box"></div>
+    <div class="box"></div>
+    <div class="box"></div>
+  </div>
+  <div id="pwa-toast" class="pwa-toast">
+    <div class="pwa-toast-wrapper">
+      <div class="pwa-toast-content">
+        <div class="message">
+          <span id="toast-message">hello</span>
+        </div>
+      </div>
+      <div class="pwa-toast-actions">
+        <button id="pwa-refresh">Refresh</button>
+        <button id="pwa-close">Close</button>
+      </div>
+    </div>
+  </div>
 `;
+
 initPWA(app);
